22793be78667349576e69d6c0fb1b25d0fcfe607
Ravi Korat <ravik.tagline@gmail.com>
2021-09-21 12:05:46 +0530

    Add swagger docs in drf

--- DRF/urls.py
+++ DRF/urls.py
@@ -17,8 +17,25 @@ from django.contrib import admin
 from django.urls import path, include
 #from django.urls.conf import include
 import demo
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Docs",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@xyz.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('demo/', include('demo.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
